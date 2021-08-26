dfa0bb68108cc1dfb8688049c0691ecf27011d76
k-shindo <k-shindo@media-plex.net>
2021-08-26 17:36:07 +0900

    update: Pre-rendering and Data Fetching

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,18 @@ import Layout, { siteTitle } from '../components/layout'
 import Link from 'next/link'
 import utilStyles from '../styles/utils.module.scss'
 
-export default function Home() {
+import { getSortedPostsData } from '../lib/post'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -20,6 +31,22 @@ export default function Home() {
       <Link href="/posts/first-post">
         <a>first-post</a>
       </Link>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+
+      </section>
     </Layout>
   )
 }
